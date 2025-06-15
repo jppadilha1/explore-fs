53cc957e75c55a132a43fd64827a40bde4655f9e
jppadilha1 <100085279+jppadilha1@users.noreply.github.com>
2025-06-15 18:31:05 +0000

    feat: generate token with `jsonwebtoken`

--- src/middleware/is-auth.js
+++ src/middleware/is-auth.js
@@ -1,7 +1,11 @@
+import jwt from "jsonwebtoken";
+
 export function isAuth(req, reply, done) {
   const { authorization } = req.headers;
 
-  if (authorization != "token") {
+  const verify = jwt.verify(authorization, process.env.JWT_SECRET);
+
+  if (!verify) {
     return reply.status(403).send("Unauthorized");
   } else {
     done(); // its same as next in express
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,16 +1,19 @@
 import { localdb } from "../../infra/localdb.js";
+import jwt from "jsonwebtoken";
 
 export function loginUser(app) {
   app.post("/access", (req, reply) => {
     const { email } = req.body;
 
     const user = localdb.findUser(email);
-    const token = "token";
 
     if (!user) {
       return reply.status(500).send("User undefined");
-    } else {
-      return reply.status(200).send(token);
     }
+
+    const token = jwt.sign({ user: user.name }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return reply.status(200).send(token);
   });
 }
